7ce1db92746415f74059c0f0fa00ecf2bb2bcfa3
PetteriRauhala <122267930+PetteriRauhala@users.noreply.github.com>
2023-05-22 14:55:09 +0300

    jaajajajajajajajajaaa

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//import { StatusBar } from 'expo-status-bar';
+//import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import React from 'react';
+import {View, Text, Image, ScrollView, TextInput, StatusBar} from 'react-native';
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <StatusBar/>
+      <Text> Jotahi tuherrusta</Text>
+      <View>
+        <Text> Lissää jotahi tuherrusta</Text>
+        <Image
+          source={{
+            uri: 'https://im.indiatimes.in/content/2023/Apr/Untitled-design---2023-04-13T132449024_6437b559b9d11.png?w=1200&h=900&cc=1',
+          }}
+          style={{width: 400, height: 400}}
+        />
+      </View>
+      <TextInput
+        style={{
+          height: 50,
+          borderColor: 'green',
+          borderWidth: 2,
+        }}
+        defaultValue=" Tuherrappa tännekki jotahi "
+      />
+    </ScrollView>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+// export default function App() {
+//  return (
+//    <View style={styles.container}>
+//      <Text>perkele</Text>
+//      <StatusBar style="auto" />
+//    </View>
+//  );
+//}
+
+//const styles = StyleSheet.create({
+//  container: {
+//    flex: 1,
+//    backgroundColor: '#fff',
+//    alignItems: 'center',
+//    justifyContent: 'center',
+//  },
+//});
+ 
